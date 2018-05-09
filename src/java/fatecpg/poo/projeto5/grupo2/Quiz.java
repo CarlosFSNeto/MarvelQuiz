862dfe656b688cf2b38352d77a2073b4b7b8c734
Outline <carlosneto@outlook.com>
2018-05-09 12:49:15 -0300

    Continuação das classes Quiz e User.

--- src/java/fatecpg/poo/projeto5/grupo2/Quiz.java
+++ src/java/fatecpg/poo/projeto5/grupo2/Quiz.java
@@ -1,17 +1,74 @@
 package fatecpg.poo.projeto5.grupo2;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Quiz {
     private static Question[] questionList = new Question[]{
-        //TODO Cadastrar as perguntas conforme a linha abaixo, separando por vírgula
+        //TODO Cadastrar as perguntas conforme as linhas abaixo:
+        new Question("Qual herói da Marvel utiliza um martelo?", "Thor", new String[]{"Homem-Aranha", "Chapolin", "Capitão América"}),
+        new Question("Qual herói da Marvel utiliza um martelo?", "Thor", new String[]{"Homem-Aranha", "Chapolin", "Capitão América"}),
+        new Question("Qual herói da Marvel utiliza um martelo?", "Thor", new String[]{"Homem-Aranha", "Chapolin", "Capitão América"}),
+        new Question("Qual herói da Marvel utiliza um martelo?", "Thor", new String[]{"Homem-Aranha", "Chapolin", "Capitão América"}),
+        new Question("Qual herói da Marvel utiliza um martelo?", "Thor", new String[]{"Homem-Aranha", "Chapolin", "Capitão América"}),
+        new Question("Qual herói da Marvel utiliza um martelo?", "Thor", new String[]{"Homem-Aranha", "Chapolin", "Capitão América"}),
+        new Question("Qual herói da Marvel utiliza um martelo?", "Thor", new String[]{"Homem-Aranha", "Chapolin", "Capitão América"}),
+        new Question("Qual herói da Marvel utiliza um martelo?", "Thor", new String[]{"Homem-Aranha", "Chapolin", "Capitão América"}),
+        new Question("Qual herói da Marvel utiliza um martelo?", "Thor", new String[]{"Homem-Aranha", "Chapolin", "Capitão América"}),
         new Question("Qual herói da Marvel utiliza um martelo?", "Thor", new String[]{"Homem-Aranha", "Chapolin", "Capitão América"}),
         new Question("Qual herói da Marvel utiliza um martelo?", "Thor", new String[]{"Homem-Aranha", "Chapolin", "Capitão América"})
     };
-    private ArrayList<Question> test;
     
-    public ArrayList<Question> getTest(){
-        //TODO retornar um ArrayList com 10 elementos sorteados do array Question
+    private static ArrayList<Quiz> getTopTenQuizzes(){
+        ArrayList<Quiz> ret = new ArrayList<>();
+        //TODO ordernar os quizzes realizados por nota; recuperá-los através da User.userList
         return null;
     }
+    
+    private static ArrayList<Quiz> getLastestTenQuizzes(){
+        //TODO ordenar os quizzes por ordem decrescente de data
+        //Outra opção: Manter uma lista que é atualizada cada vez que um Quiz é finalizado
+        return null;
+    }
+    private ArrayList<Question> test;
+    private double grade;
+    private Date date;
+
+//    public static void main(String[] args){
+//        Quiz q = new Quiz();
+//        for(Question item: q.test){
+//            System.out.println(item.getQuestion());
+//        }
+//        System.out.println(q.date.toLocaleString());
+//    }
+    public Quiz() {
+        //A nota deve ser atualizada com o método setGrade quando o usuário terminar o teste
+        grade = 0d;
+        date = Calendar.getInstance().getTime();
+        test = new ArrayList<>(10);
+        for (int i=0; i<10; i++){
+            int randomIndex;
+            do {
+                randomIndex = (int)(Math.random() * questionList.length);
+            }while (test.contains(questionList[randomIndex]));
+            test.add(questionList[randomIndex]);
+        }
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+        //Ao definir a nota final, atualiza a hora:
+        this.date = Calendar.getInstance().getTime();
+    }
+
+    public String getDate() {
+        return date.toLocaleString();
+    }
+
+    
 }
--- src/java/fatecpg/poo/projeto5/grupo2/User.java
+++ src/java/fatecpg/poo/projeto5/grupo2/User.java
@@ -12,6 +12,28 @@ import java.util.ArrayList;
  * @author Outline
  */
 public class User {
+    public static ArrayList<User> userList = new ArrayList<>();
+    
     private String name;
     private ArrayList<Quiz> finishedQuizzes;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Quiz> getFinishedQuizzes() {
+        return finishedQuizzes;
+    }
+    
+    public double getUserAverageGrade(){
+        double sum = 0;
+        for(Quiz q:finishedQuizzes){
+            sum += q.getGrade();
+        }
+        return sum / (double)finishedQuizzes.size();
+    }
 }
