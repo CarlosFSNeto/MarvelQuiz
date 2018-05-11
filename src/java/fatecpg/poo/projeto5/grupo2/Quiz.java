5778af42e638bb8b55b1d3f12205339b0ad32047
Junior.Cordeiro <jpc_892@hotmail.com>
2018-05-11 16:51:41 -0300

    Add Quiz

--- src/java/fatecpg/poo/projeto5/grupo2/Quiz.java
+++ src/java/fatecpg/poo/projeto5/grupo2/Quiz.java
@@ -9,15 +9,19 @@ public class Quiz {
         //TODO Cadastrar mais perguntas
         new Question("Qual herói da Marvel utiliza um martelo?", "Thor", new String[]{"Thor", "Homem-Aranha", "Chapolin", "Capitão América"}),
         new Question("Onde se passa a história do Pantera Negra?", "Wakanda", new String[]{"Nova York", "São Vicente", "Wakanda", "Nigéria"}),
+        new Question("Em que ano o Homem-Aranha foi criado?", "1952", new String[]{"1932", "1937", "1960", "1952"}),
         new Question("Quem comanda a SHIELD?", "Nick Fury", new String[]{"Nick Fury", "Capitão América", "Reed Richards", "Tony Stark"}),
         new Question("Qual atriz interpreta a Viúva Negra?", "Scarlett Johansson", new String[]{"Chloe Bennet", "Elizabeth Olsen", "Emma Watson", "Scarlett Johansson"}),
         new Question("De acordo com o site adorocinema, Joss Whedon (diretor de diversos filmes da UCM, como Os Vingadores) disse que uma série importante da Marvel foi criada contra a vontade do estúdio. Que série foi essa?", "Agents of SHIELD", new String[]{"Jéssica Jones", "Demolidor", "Agent Carter", "Agents of SHIELD"}),
+        new Question("Qual a idade do Homem-Aranha?", "15", new String[]{"16,5", "16", "15", "14"}),
+        new Question("Quantas são as jóias do infinito?", "6", new String[]{"6", "9", "5", "4"}),
         new Question("Atualmente (abril de 2016), o Homem Aranha foi recuperado pela Marvel. Antes, seus direitos estavam com quem?", "Sony", new String[]{"DC", "Fox", "Sony", "Warner Bros"}),
         new Question("Qual foi a primeira super heroína com poderes inumanos a aparecer no UCM?", "Daisy Johnson", new String[]{"Viúva Negra", "Feiticeira Escarlate", "Daisy Johnson", "Harpia"}),
         new Question("Stan Lee aparece em quase todos os filmes da Marvel. Onde ele aparece em Os Vingadores?", "Em um telejornal", new String[]{"Em um telejornal", "Em um asilo", "Em um leilão", "Vendendo cachorro-quente"}),
         new Question("Qual destes NÃO é um vilão da Marvel?", "General Zod", new String[]{"Caveira Vermelha", "General Zod", "MODOK", "Arnim Zola"}),
         new Question("Qual mutante que, assim como Wolverine, tem o poder de cura?", "Deadpool", new String[]{"Magneto", "Noturno", "Ciclope", "Deadpool"}),
         new Question("Quem foi o primeiro vingador?", "Capitão América", new String[]{"Capitão América", "Homem-Aranha", "Homem de Ferro", "Hulk"}),
+        new Question("Qual a ciadade do Homem-Aranha?", "Nova Iorque", new String[]{"Nova Iorque", "Filadélfia", "Los Angeles", "Detroit"}),
  
     };
     
